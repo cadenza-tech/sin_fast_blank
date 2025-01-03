8e2dc7d96aa0e8c4e8185827332c911534538061
Masahiro <watanabe@cadenza-tech.com>
2025-01-04 02:33:44 +0900

    Refactor fast_blank.rb

--- lib/fast_blank.rb
+++ lib/fast_blank.rb
@@ -1,12 +1,19 @@
+# frozen_string_literal: true
+
 class ::String
   # Explicitly undefine method before redefining to avoid Ruby warnings.
   undef_method(:blank?) if method_defined?(:blank?)
 end
 
 case RUBY_ENGINE
-  when 'jruby'
-    require 'fast_blank.jar'
-    JRuby::Util.load_ext("com.headius.jruby.fast_blank.FastBlankLibrary")
+when 'jruby'
+  require 'fast_blank.jar'
+
+  JRuby::Util.load_ext('com.headius.jruby.fast_blank.FastBlankLibrary')
+else
+  if RUBY_PLATFORM.include?('darwin')
+    require 'fast_blank.bundle'
   else
     require 'fast_blank.so'
+  end
 end
