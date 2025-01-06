b4afb2f194a8f19f26f9cf2d98261c089c56acd9
Masahiro <watanabe@cadenza-tech.com>
2025-01-06 23:30:53 +0900

    Change lib_dir

--- lib/sin_fast_blank.rb
+++ lib/sin_fast_blank.rb
@@ -12,9 +12,9 @@ when 'jruby'
   JRuby::Util.load_ext('sin_fast_blank.SinFastBlankLibrary')
 else
   if RUBY_PLATFORM.include?('darwin')
-    require 'sin_fast_blank.bundle'
+    require 'sin_fast_blank/sin_fast_blank.bundle'
   else
-    require 'sin_fast_blank.so'
+    require 'sin_fast_blank/sin_fast_blank.so'
   end
 end
 
