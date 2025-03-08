99ef4f50ebe633ae78c2116b0dedc8f5b183bafc
Masahiro <watanabe@cadenza-tech.com>
2025-03-09 02:32:54 +0900

    Fix test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,8 @@ $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'active_support/core_ext/object/blank'
+
+class String
+  alias_method :as_blank?, :blank?
+end
