8ae9e1f84848042b73a18a8cf5ecafc793472132
Masahiro <watanabe@cadenza-tech.com>
2025-08-12 22:53:50 +0900

    Refactor tests

--- test/test_ascii_blank.rb
+++ test/test_ascii_blank.rb
@@ -4,46 +4,68 @@ require_relative 'test_helper'
 require 'sin_fast_blank'
 
 class TestAsciiBlank < Minitest::Test
-  def test_equivalency # rubocop:disable Metrics/MethodLength
-    [
-      '',
-      ' ',
-      '	',
-      "\r\n",
-      "\t\n\v\f\r\s 	",
-      "\t\n\v\f\r\s 	\t\n\v\f\r\s 	\t\n\v\f\r\s 	\t\n\v\f\r\s 	Lorem ipsum",
-      '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 🐈️', # rubocop:disable Layout/LineLength
-      '    吾輩は猫である。名前はまだ無い。',
-      '    吾輩は🐈️である。名前はまだ無い。',
-      '🐈️',
-      '    🐈️'
-    ].each do |s|
-      assert_equal(!!(s =~ /\A[[:space:]]*\z/), s.ascii_blank?) # rubocop:disable Style/DoubleNegation
-    end
+  TEST_STRINGS = [
+    '',
+    ' ',
+    '	',
+    "\r\n",
+    "\t\n\v\f\r\s 	",
+    "\t\n\v\f\r\s 	\t\n\v\f\r\s 	\t\n\v\f\r\s 	\t\n\v\f\r\s 	Lorem ipsum",
+    '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 🐈️', # rubocop:disable Layout/LineLength
+    '    吾輩は猫である。名前はまだ無い。',
+    '    吾輩は🐈️である。名前はまだ無い。',
+    '🐈️',
+    '    🐈️'
+  ].freeze
+  UTF8_CODEPOINT_MAX = 0xFFFF
+  ASCII_CODEPOINT_MAX = 0xFF
 
-    (16 * 16 * 16 * 16).times do |i|
-      begin
-        c = i.chr('UTF-8')
-      rescue StandardError
-        next
-      end
-
-      assert_equal(c.strip.empty?, c.ascii_blank?)
-    end
-
-    256.times do |i|
-      begin
-        c = i.chr('ASCII')
-      rescue StandardError
-        next
-      end
-
-      assert_equal(c.strip.empty?, c.ascii_blank?)
-    end
+  def test_equivalency
+    test_strings_equivalency
+    test_utf8_chars_equivalency
+    test_ascii_chars_equivalency
   end
 
   def test_null_character
     assert_predicate("\u0000", :ascii_blank?)
+  end
+
+  private
+
+  def test_strings_equivalency
+    TEST_STRINGS.each do |s|
+      expected = ascii_space_only?(s)
+
+      assert_equal(expected, s.ascii_blank?)
+    end
+  end
+
+  def test_utf8_chars_equivalency
+    UTF8_CODEPOINT_MAX.times do |i|
+      char = safe_chr(i, 'UTF-8')
+      next unless char
+
+      assert_equal(char.strip.empty?, char.ascii_blank?)
+    end
+  end
+
+  def test_ascii_chars_equivalency
+    ASCII_CODEPOINT_MAX.times do |i|
+      char = safe_chr(i, 'ASCII')
+      next unless char
+
+      assert_equal(char.strip.empty?, char.ascii_blank?)
+    end
+  end
+
+  def ascii_space_only?(str)
+    !!(str =~ /\A[[:space:]]*\z/)
+  end
+
+  def safe_chr(codepoint, encoding)
+    codepoint.chr(encoding)
+  rescue StandardError
+    nil
   end
 end
